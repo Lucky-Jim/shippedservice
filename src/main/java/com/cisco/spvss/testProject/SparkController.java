73c5a4d10a9fabe3f760145b5bb88115ac8d5df4
Tom Burnley <tburnley@cisco.com>
2016-09-20 15:18:57 +0100

    Add Spark message logging

--- src/main/java/com/cisco/spvss/testProject/SparkController.java
+++ src/main/java/com/cisco/spvss/testProject/SparkController.java
@@ -1,9 +1,15 @@
 package com.cisco.spvss.testProject;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +17,25 @@ public class SparkController {
 
 	private static final Logger tom_logger = LoggerFactory.getLogger(SparkController.class);
 	
-	@RequestMapping("/spark/{name}")
-	public String sparkHook(@PathVariable String name) {
+	private static LinkedList<SparkMessage> messages = new LinkedList<SparkMessage>();
+	
+	
+	@RequestMapping("/spark/test")
+	public String sparkHook( @RequestBody SparkMessage message) {
 		tom_logger.info("Incoming Sparks request");
 
+		messages.add( message );
 			
 		return "This is a String";
 	}
+	
+	@RequestMapping("/spark/messages")
+	public @ResponseBody ResponseEntity<List<SparkMessage>> getMessages( ) {
+		tom_logger.info("Incoming List request");
+
+			
+		 return new ResponseEntity<List<SparkMessage>> (messages, HttpStatus.OK); // XXX
+	}
+	
+	
 }
---
+++ src/main/java/com/cisco/spvss/testProject/SparkMessageData.java
@@ -0,0 +1,50 @@
+package com.cisco.spvss.testProject;
+
+
+/**
+ * 
+ * @author tburnley
+ *
+ *"data":{
+    "id":"Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj",
+    "roomId":"Y2lzY29zcGFyazovL3VzL1JPT00vY2RlMWRkNDAtMmYwZC0xMWU1LWJhOWMtN2I2NTU2ZDIyMDdi",
+    "personId":"Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lM2EyNjA4OC1hNmRiLTQxZjgtOTliMC1hNTEyMzkyYzAwOTg",
+    "personEmail":"person@example.com",
+    "created":"2015-12-04T17:33:56.767Z"
+  }
+ */
+public class SparkMessageData {
+
+		private String id;
+		
+		private String roomId;
+		
+		private String personId;
+		
+		private String personEmail;
+		
+		private String created;
+		
+		public String getId() {
+			return id;
+		}
+
+		public String getPersonID() {
+			return personId;
+		}
+		
+		public String getRoomID() {
+			return roomId;
+		}
+		
+		public String getPersonEmail() {
+			return personEmail;
+		}
+		
+		public String getCreatedDate() {
+			return created;
+		}
+		
+		
+		
+}
